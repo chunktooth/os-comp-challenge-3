9fe8791a94602ccd6f9a7f43b9fca771e4cb76fe
chunktooth <om.saetehn@gmail.com>
2018-01-12 14:09:09 -0700

    Media queries

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-$('.toggle-btn').toggle(function() {
-    $('.left-pane').attr('hidden', true);
-}, function() {
-    $('.left-pane').attr('hidden', false);
+$('.toggle-nav-btn').on('click', function(){
+    $('.left-pane').toggle('slide', { direction: 'left' }, 1000);
+    $('.right-pane').animate({
+        'margin-left' : $('.right-pane').css('margin-left') == '0px' ? '260px' : '0px'
+    }, 1000);
+    console.log('hey!');
 });
